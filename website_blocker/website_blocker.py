5be5a1b19b2eb6b2c4f8daf2017fe790d50e811e
manika15 <manika.kapoor90@gmail.com>
2017-08-09 18:35:00 -0700

    program to block certain sites on particular time

---
+++ website_blocker/website_blocker.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp = "hosts"
+hosts_path = "/etc/hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com","www.gmail.com", "gmail.com", "www.mail.google.com/mail", "mail.google.com/mail"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
+        print ("Working Hours..")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        print ("fun Hours")
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+    time.sleep(5)
+
+#add this in sudo crontab -e to run the program when system reboots
+#@reboot python3 /home/PycharmProjects/python_mega_course_udemy/website_blocker/website_blocker.py
