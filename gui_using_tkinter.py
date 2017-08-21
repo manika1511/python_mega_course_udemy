db8c2b2be5c09361d4e4c373aa40263915dd52ad
manika15 <manika.kapoor90@gmail.com>
2017-08-20 23:53:02 -0700

    add function to be executed, text displayed

--- gui_using_tkinter.py
+++ gui_using_tkinter.py
@@ -1,10 +1,16 @@
 from tkinter import *
 
 window=Tk()
-b1=Button(window, text="Enter")
+
+def km_to_miles():
+    miles=float(input_val.get())*1.6
+    t1.insert(END, miles)
+
+b1=Button(window, text="Convert", command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1=Entry(window)
+input_val=StringVar()
+e1=Entry(window, textvariable=input_val)
 e1.grid(row=0, column=1)
 
 t1=Text(window, height=1, width=10)
