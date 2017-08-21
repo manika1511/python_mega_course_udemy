daba1468b2ee75ac8341ce7e9a8fd6ae35857bb2
manika15 <manika.kapoor90@gmail.com>
2017-08-21 00:28:03 -0700

    refactor the code, add separate functions for create table and insert queries

--- databases/sqlite.py
+++ databases/sqlite.py
@@ -2,18 +2,22 @@
 
 import sqlite3
 
-#Step1: Connecting to Database
-conn=sqlite3.connect("lite.db")  #if this file exists, then the connection is made with it, else new db created
+def create_table():
+    #Step1: Connecting to Database
+    conn=sqlite3.connect("lite.db")  #if this file exists, then the connection is made with it, else new db created
+    #Step2: Create cursor object to access the rows in database
+    cur=conn.cursor()
+    #Step3: SQL Query
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, qty INTEGER, price FLOAT)")
+    #Step4: Commit changes to the database
+    conn.commit()
+    #Step5: Close the connection
+    conn.close()
 
-#Step2: Create cursor object to access the rows in database
-cur=conn.cursor()
+def insert(item, qty, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,? )",(item, qty, price))
+    conn.commit()
+    conn.close()
 
-#Step3: SQL Query
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, qty INTEGER, price FLOAT)")
-cur.execute("INSERT INTO store VALUES('Yogurt', 10, 3.56 )")
-
-#Step4: Commit changes to the database
-conn.commit()
-
-#Step5: Close the connection
-conn.close()
