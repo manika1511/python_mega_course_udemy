5d840d8768312b51f251ffd7fcab4a1db5730432
manika15 <manika.kapoor90@gmail.com>
2017-08-21 00:18:14 -0700

    add an entry in the table

--- databases/sqlite.py
+++ databases/sqlite.py
@@ -10,6 +10,7 @@ cur=conn.cursor()
 
 #Step3: SQL Query
 cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, qty INTEGER, price FLOAT)")
+cur.execute("INSERT INTO store VALUES('Yogurt', 10, 3.56 )")
 
 #Step4: Commit changes to the database
 conn.commit()
