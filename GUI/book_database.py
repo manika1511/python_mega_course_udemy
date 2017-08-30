413ffe3ec24cb247e1767acfd6bcb91fadc86d7d
manika15 <manika.kapoor90@gmail.com>
2017-08-29 17:01:12 -0700

    add the problem statement/layout for the GUI

---
+++ GUI/book_database.py
@@ -0,0 +1,15 @@
+"""
+Program to store following book information:
+Title,Author
+Year,ISBN
+
+User can:
+View records
+Search an entry
+Add entry
+Update Entry
+Delete 
+Close   
+
+Also there will be text boxes to enter and a scroll bar
+"""
