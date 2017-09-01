e94a85bc07754ccd0ae6f17a7737ca2406df652d
manika15 <manika.kapoor90@gmail.com>
2017-09-01 16:53:48 -0700

    add image stats

--- computer_vision/image_processing.py
+++ computer_vision/image_processing.py
@@ -2,3 +2,8 @@ import cv2
 
 #read the image in grayscale.. 0 shows grayscale; 1 shows rbg scale
 img=cv2.imread("galaxy.jpg", 0)
+
+#shape of image: 1485X990
+print (img.shape)
+#no. of dimensions
+print (img.ndim)
